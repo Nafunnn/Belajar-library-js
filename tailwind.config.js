4805c0db1354e4be107ca0dbac4e34eecde13192
Nafunnn <aanalim31@gmail.com>
2023-05-27 21:03:01 +0700

    [+] add theme generator [+] content at vantajs page [=] fixing navbar at vantajs page

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,31 @@ module.exports = {
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#ff4b57",
+
+          secondary: "#152747",
+
+          accent: "#513448",
+
+          neutral: "#171618",
+
+          "base-100": "#09090B",
+
+          info: "#66C6FF",
+
+          success: "#87D039",
+
+          warning: "#E2D562",
+
+          error: "#FF6F6F",
+        },
+      },
+    ],
+  },
   plugins: [
     require("daisyui"),
     require("flowbite/plugin"),
